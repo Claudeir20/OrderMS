6aa37b8910810da9593daaa53d0e36ee3fd7d5ff
Claudeir <claudeirmota2@gmail.com>
2026-03-06 11:27:36 -0300

    feat: add OrderEntity JPA mapping

---
+++ OrderMS/src/main/java/com/example/OrderMS/infra/model/OrderEntity.java
@@ -0,0 +1,44 @@
+package com.example.OrderMS.infra.model;
+
+import com.example.OrderMS.core.entities.enums.StatusEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private BigDecimal productPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusEnum status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+}
