5ab3a2f7942640b2921b444fb3733f5ae3f3f946
Claudeir <claudeirmota2@gmail.com>
2026-03-10 17:04:27 -0300

    feat: add  new Exception

---
+++ OrderMS/src/main/java/com/example/OrderMS/core/exceptions/OrderInvalidStateException.java
@@ -0,0 +1,7 @@
+﻿package com.example.OrderMS.core.exceptions;
+
+public class OrderInvalidStateException extends DomainException {
+    public OrderInvalidStateException(String message) {
+        super(message);
+    }
+}
---
+++ OrderMS/src/main/java/com/example/OrderMS/core/exceptions/OrderValidationException.java
@@ -0,0 +1,7 @@
+﻿package com.example.OrderMS.core.exceptions;
+
+public class OrderValidationException extends DomainException {
+    public OrderValidationException(String message) {
+        super(message);
+    }
+}
